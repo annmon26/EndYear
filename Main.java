16d6c033ce42046ac3665ee2b41eba85f874f094
annmon26 <annmon26@bergen.org>
2025-05-19 11:26:32 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
 public class Main{
-  
+   public static void main(String[] args) throws FileNotFoundException {
+        Scanner in = new Scanner();
+       System.out.println("hello world");
+}
 }
